7e28cff8527ee966c23749bedc2521d33dd03b34
Pan0s13sif <98046632+Pan0s13sif@users.noreply.github.com>
2022-03-15 21:58:40 +0200

    Created add row functionality for table

--- table.js
+++ table.js
@@ -1,1 +1,18 @@
+function addRow() {
+    let name = document.getElementById("nameId").value;
+    let property = document.getElementById("propertyId").value;
+    let size = document.getElementById("sizeId").value;
+    let i = document.getElementById("propertyTable").rows.length ;
 
+    if (!$.isNumeric(size)) {
+        alert("Invalid Size. Please enter the correct value.");
+        return;
+    }
+
+    if (name && property && size) {
+        $("#propertyTable>tbody").append("<tr><td>" + i + "</td><td> " + name + "</td><td> " + property + "</td><td>" + size + "</td><td>" +
+            "<button type=\"button\" onclick=\"removeRow(this)\" class=\"btn btn-danger\">Delete</button></td></tr>");
+    } else {
+        alert("Please fill all the required fields.");
+    }
+}
